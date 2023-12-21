43de9a90a5b4399ecdf6771bfdc7c95287fb447d
Tien Pham <tienphamvanfuv@gmail.com>
2023-12-22 00:41:19 +0100

    add CarImage in 1 big div above and 1 another big div contains 3 smaller div

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -30,6 +30,83 @@ export default function CarDetails({
           >
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="relative w-full max-w-lg max-h[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+                  <button
+                    type="button"
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                    onClick={closeModal}
+                  >
+                    <Image
+                      src="/close.svg"
+                      width={20}
+                      height={20}
+                      alt="close"
+                      className="object-contain"
+                    />
+                  </button>
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image
+                        src="/hero.png"
+                        fill
+                        priority
+                        alt="car model"
+                        className="object-contain"
+                      />
+                    </div>
+                    <div className="flex gap-3">
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100">
+                        <Image
+                          src="/hero.png"
+                          fill
+                          priority
+                          alt="car model"
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100">
+                        <Image
+                          src="/hero.png"
+                          fill
+                          priority
+                          alt="car model"
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100">
+                        <Image
+                          src="/hero.png"
+                          fill
+                          priority
+                          alt="car model"
+                          className="object-contain"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  {/* Create the car details content */}
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2 className="font-semibold text-xl capitalize">
+                      {car.make} {car.model}
+                    </h2>
+                    <div className="mt-3 flex flex-wrap gap-4"></div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
         </Dialog>
       </Transition>
     </>
