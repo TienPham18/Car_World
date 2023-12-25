e16a81010ee2e688cc68e0aa074971b0b1daf01e
Tien Pham <tienphamvanfuv@gmail.com>
2023-12-26 00:44:07 +0100

    add ListBox.Options

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -13,7 +13,7 @@ export default function CustomFilter({ options, title }: CustomFilterProps) {
     <div className="w-fit">
       <Listbox value={selected} onChange={(e) => setSelected(e)}>
         <div className="relative w-fit z-10">
-          <Listbox.Button className="block truncate">
+          <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{title}</span>
             <Image
               src="/chevron-up-down.svg"
@@ -23,6 +23,37 @@ export default function CustomFilter({ options, title }: CustomFilterProps) {
               alt="chevron up down"
             />
           </Listbox.Button>
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="custom-filter__options">
+              {options.map((option) => (
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {({ selected }) => (
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {option.title}
+                    </span>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>
