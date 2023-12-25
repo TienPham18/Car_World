67f27c254ce14d6ac6e7ce765e61ee5c95283516
Tien Pham <tienphamvanfuv@gmail.com>
2023-12-25 23:51:17 +0100

    add CustomFilterProps in CustomFilter function

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,5 +1,30 @@
+"use client";
 import React from "react";
+import { Fragment, useState } from "react";
+import { useRouter } from "next/router";
+import { Listbox, Transition } from "@headlessui/react";
+import { CustomFilterProps } from "@/types";
+import Image from "next/image";
 
-export default function CustomFilter() {
-  return <div>CustomFilter</div>;
+export default function CustomFilter({ options, title }: CustomFilterProps) {
+  const [selected, setSelected] = useState(options);
+
+  return (
+    <div className="w-fit">
+      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="block truncate">
+            <span className="block truncate">{title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+              alt="chevron up down"
+            />
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
+  );
 }
